228c2f58cd8d6e95b37b3b326cb3f2dc1b7ecec9
ujwxl23 <ujwalkr23@gmail.com>
2022-08-06 11:30:32 +0530

    Create FUNDvote.sol
    Setting variables required at start using constructor.
    fundEth function to fund in the contract.
    getTotalBalance to get balance of fund in the contract.

---
+++ FUNDvote.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+contract FUNDvote{
+    mapping(address=>uint)public contributors;
+    address public manager;
+    uint public minContribution;
+    uint public deadline;
+    uint public targetprice;
+    uint public raisedAmt;
+    uint public  no0fContributors;
+
+    constructor(uint _targetprice,uint _deadline){
+    targetprice=_targetprice;
+    deadline=block.timestamp+_deadline; // 10sec+3600sec(60*60)
+    minContribution=100 wei;
+    manager=msg.sender;
+    }
+    
+    function fundEth()public payable{
+    require(block.timestamp<deadline,"Deadline has been passed");
+    require(msg.value>=minContribution,"Minimum Contribution is not met");
+    if(contributors[msg.sender]==0){
+        no0fContributors ++;
+    }
+    contributors[msg.sender]+=msg.value;
+    raisedAmt+=msg.value;
+    }
+
+    function getTotalBalance() public view returns (uint){
+        return address(this).balance;
+    }
+
+}
