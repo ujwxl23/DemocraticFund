49761e1013843f1d69d1d2a4c0b380895e7c28c3
ujwxl23 <ujwalkr23@gmail.com>
2022-08-06 12:45:23 +0530

    Upadated PaymentProcess.sol
    adding the feature of pulling request for the manager.

--- PaymentProcess.sol
+++ PaymentProcess.sol
@@ -9,6 +9,19 @@ contract PaymentProcess{
     uint public raisedAmt;
     uint public  no0fContributors;
 
+    struct Request{
+        string description;
+        address payable recipient;
+        uint value;
+        bool completed;
+        uint no0fFVoters;
+        uint noOfNVoters;
+        mapping(address=>bool)voters;
+    }
+    mapping (uint=>Request) public requests;
+    uint public numReq;
+
+
     constructor(uint _targetprice,uint _deadline){
     targetprice=_targetprice;
     deadline=block.timestamp+_deadline; // 10sec+3600sec(60*60)
@@ -35,6 +48,23 @@ contract PaymentProcess{
         require(contributors[msg.sender]>0);
         address payable user= payable (msg.sender);
         user.transfer(contributors[msg.sender]);
+        contributors[msg.sender]=0;
+    }
+
+    modifier byManagerOnly(){
+        require(msg.sender==manager,"Manager function only");
+        _;
+    }
+
+    function createRequest(string memory _description, address payable _recipient, uint _value) public byManagerOnly{
+        Request storage newRequest = requests[numReq];
+        numReq ++;
+        newRequest.description=_description;
+        newRequest.recipient=_recipient;
+        newRequest.value=_value;
+        newRequest.completed=false;
+        newRequest.no0fFVoters=0;
+        newRequest.noOfNVoters=0;
     }
 
 }
